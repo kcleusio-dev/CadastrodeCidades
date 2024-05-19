116925b68e64d3400152c7e5598a9b94004a0c54
kcleusio-dev <kcleusio-dev>
2024-05-19 07:55:05 +0100

    feat: Adicionado o MUI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
 
   return (
     <Routes>
-      <Route path="/home" element={<p>Página Inicial</p>} />
+      <Route path="/home" element={<Button>Adicionar</Button>} />
 
       <Route path="*" element={<Navigate to="/home" />} />
     </Routes>
