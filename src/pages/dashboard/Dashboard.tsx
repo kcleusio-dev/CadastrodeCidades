c9e9bddcbb91e3b9a0bc92a54a360dfe9466c4ca
kcleusio-dev <kcleusio.dev@gmail.com>
2024-05-25 17:34:20 +0100

    feat: Add ferramentas de detalhes

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,15 +1,15 @@
+import { FerramentasDeDetalhes } from "../../shared/components";
 import { FerramentasDaListagem } from "../../shared/components/ferramentas-da-listagem/FerramentasDaListagem";
 import { LayoutBaseDePagina } from "../../shared/layouts"
 
 export const Dashboard = () => {
   return (
     <LayoutBaseDePagina
-      titulo="Página Inicial"
+      titulo="Página Incial"
       barraDeFerramentas={(
-        <FerramentasDaListagem
-          mostrarInputBusca />
+        <FerramentasDeDetalhes />
       )}>
-      Testando
-    </LayoutBaseDePagina>
+        Testandoooo
+    </LayoutBaseDePagina >
   );
 }
--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,2 +1,3 @@
 export * from './ferramentas-da-listagem/FerramentasDaListagem';
 export * from './menu-lateral/MenuLateral';
+export * from './ferramentas-de-detalhe/FerramentasDeDetalhe'
